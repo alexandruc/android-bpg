338d72f755e65fcffd0c0bfe8335820e1afc11e0
alex <chica.alexandru@gmail.com>
2015-05-23 21:43:26 +0300

    Add decode from buffer to buffer

--- Android-BPG-client/jni/bpg-utils/decoder.c
+++ Android-BPG-client/jni/bpg-utils/decoder.c
@@ -14,7 +14,7 @@
 
 #include "libbpg.h"
 
-static void ppm_save(BPGDecoderContext *img, const char *filename)
+static void ppm_save_to_file(BPGDecoderContext *img, const char *filename)
 {
     BPGImageInfo img_info_s, *img_info = &img_info_s;
     FILE *f;
@@ -43,6 +43,47 @@ static void ppm_save(BPGDecoderContext *img, const char *filename)
     }
     fclose(f);
 
+    free(rgb_line);
+}
+
+static void ppm_save_to_buffer(BPGDecoderContext *img, uint8_t** outBuf, unsigned int *outBufLen)
+{
+    BPGImageInfo img_info_s, *img_info = &img_info_s;
+    int w, h, y, size_of_line, size_of_header, bufferIncrement;
+    uint8_t *rgb_line;
+    char header[255];
+
+    memset(header, 0, 255*sizeof(char));
+    bpg_decoder_get_info(img, img_info);
+
+    w = img_info->width;
+    h = img_info->height;
+    size_of_line = 3*w;
+    rgb_line = malloc(size_of_line);
+    if(NULL == rgb_line){
+        printf("FAILED to allocate \n");
+        return;
+    }
+
+    snprintf(header, 255, "P6\n%d %d\n%d\n", w, h, 255);
+    *outBufLen = size_of_line*h + strlen(header) /*+1*/;
+    *outBuf = (uint8_t*)malloc( *outBufLen );
+    if(NULL == *outBuf){
+        printf("FAILED to allocate \n");
+        free(rgb_line);
+        return;
+    }
+    //copy the header first
+    size_of_header = strlen(header)*sizeof(char);
+    memcpy(*outBuf, header, size_of_header);
+
+    bpg_decoder_start(img, BPG_OUTPUT_FORMAT_RGB24);
+    bufferIncrement = 0;
+    for (y = 0; y < h; y++) {
+        bpg_decoder_get_line(img, rgb_line);
+        memcpy( (*outBuf)+size_of_header+bufferIncrement, rgb_line, size_of_line);
+        bufferIncrement += size_of_line;
+    }
     free(rgb_line);
 }
 
@@ -231,16 +272,45 @@ static void bpg_show_info(const char *filename, int show_extensions)
     }
 }
 
-void decode(char* bpgFilename, char* outFilename){
+void decode_buffer(uint8_t* bufIn, unsigned int bufInLen, uint8_t** bufOut, unsigned int* bufOutLen){
+    BPGDecoderContext *img;
+    uint8_t *buf;
+    int buf_len, bit_depth;
+
+    if(NULL == bufIn || bufInLen==0 || NULL == bufOut){
+        printf("Invalid input data \n");
+        return;
+    }
+
+    img = bpg_decoder_open();
+
+    if (bpg_decoder_decode(img, bufIn, bufInLen) < 0) {
+        fprintf(stderr, "Could not decode image\n");
+        return;
+    }
+
+#ifdef USE_PNG
+    p = strrchr(outfilename, '.');
+    if (p)
+        p++;
+
+    if (p && strcasecmp(p, "ppm") != 0) {
+        png_save(img, outfilename, bit_depth);
+    } else
+#endif
+    {
+        ppm_save_to_buffer(img, bufOut, bufOutLen);
+    }
+
+    bpg_decoder_close(img);
+}
+
+void decode_file(char* bpgFilename, char* outFilename){
     FILE *f;
     BPGDecoderContext *img;
     uint8_t *buf;
     int buf_len, bit_depth;
     const char *outfilename, *filename, *p;
-
-    if(!bpgFilename || !outFilename){
-    	return;
-    }
 
     f = fopen(filename, "rb");
     if (!f) {
@@ -278,8 +348,54 @@ void decode(char* bpgFilename, char* outFilename){
     } else
 #endif
     {
-        ppm_save(img, outfilename);
+        ppm_save_to_file(img, outfilename);
     }
 
     bpg_decoder_close(img);
 }
+
+#if 0
+int main(int argc, char **argv)
+{
+    FILE *f;
+    uint8_t *buf;
+    uint8_t *decBuf;
+    unsigned int buf_len, decBuf_len;
+
+    if(argc < 2){
+        printf("Usage: \n libbpg-decode <input file> <ouput file> \n");
+        exit(1);
+    }
+
+    f = fopen(argv[1], "rb");
+    if (!f) {
+        fprintf(stderr, "Could not open %s\n", argv[1]);
+        exit(1);
+    }
+
+    fseek(f, 0, SEEK_END);
+    buf_len = ftell(f);
+    fseek(f, 0, SEEK_SET);
+
+    buf = malloc(buf_len);
+    if (fread(buf, 1, buf_len, f) != buf_len) {
+        fprintf(stderr, "Error while reading file\n");
+        exit(1);
+    }
+
+    fclose(f);
+
+    decode_buffer(buf, buf_len, &decBuf, &decBuf_len);
+
+    f = fopen(argv[2], "wb");
+    if (!f) {
+        fprintf(stderr, "Could not open %s\n", argv[2]);
+        exit(1);
+    }
+
+    fwrite(decBuf, 1, decBuf_len, f);
+    fclose(f);
+
+    return 0;
+}
+#endif
