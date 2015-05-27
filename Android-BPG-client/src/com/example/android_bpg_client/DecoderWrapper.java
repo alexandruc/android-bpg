517591df6d8ea324d054b28eaea9e1158eb5ee72
Ionut Negru <ionut.negru@cynny.com>
2015-05-27 10:50:19 +0300

    Updated project to the latest gradle version

--- Android-BPG-client/src/com/example/android_bpg_client/DecoderWrapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.android_bpg_client;
-
-public class DecoderWrapper {
-	
-	public static native int fetchDecodedBufferSize(byte[] encBuffer, int encBufferSize);
-	
-	public static native byte[] decodeBuffer(byte[] encBuffer, int encBufferSize);
-
-}
